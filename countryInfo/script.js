76b70ea980273f639a766ff0b6c6ae64becfe9f5
itsmanvendra <singh03manvendra@gmail.com>
2023-03-06 23:48:10 +0530

    update countryInfo

--- countryInfo/script.js
+++ countryInfo/script.js
@@ -4,16 +4,10 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
-const display = function(country_name){
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v2/name/${country_name}`);
-    request.send();
-    request.addEventListener('load', function(){
-        // console.log(JSON.parse(this.responseText));
-        const [country] = JSON.parse(this.responseText);
-        const html = `
-            <article class="country">
+const displayCountry = function(country, className = ''){
+    console.log(country);
+    const html = `
+            <article class="country ${className}">
             <img class="country__img" src="${country.flag}" />
             <div class="country__data">
                 <h3 class="country__name">${country.name}</h3>
@@ -26,13 +20,32 @@ const display = function(country_name){
         `
         countriesContainer.insertAdjacentHTML('beforeend', html);
         countriesContainer.style.opacity = 1;
+}
+const display = function(country_name){
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v2/name/${country_name}`);
+    request.send();
+    request.addEventListener('load', function(){
+        // console.log(JSON.parse(this.responseText));
+        const [country] = JSON.parse(this.responseText);
+        const [border] = country.borders;
+        // console.log(border)
+        displayCountry(country);
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.com/v2/alpha/${border}`);
+        request2.send();
+        request2.addEventListener('load', function(){
+            const country = JSON.parse(this.responseText);
+            displayCountry(country, 'neighbour');
+        })
+        
 
     });
 }
 
 display('canada');
-display('germany');
-display('france');
+// display('germany');
+
 
 
 
